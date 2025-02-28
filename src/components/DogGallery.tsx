e3f0aaa044bdc808c85d573e3213fbfc725e5494
AbduraufAslanov-coder <abduaslanov2013@gmail.com>
2025-02-28 20:05:12 +0400

    'salaam

--- src/components/DogGallery.tsx
+++ src/components/DogGallery.tsx
@@ -1,21 +1,27 @@
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Spinner, Text } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { Image } from '@chakra-ui/react';
 
 const DogGallery = () => {
     const [dog, setDog] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         fetch('https://dog.ceo/api/breeds/image/random/6')
             .then(res => res.json())
-            .then(data => setDog(data.message));
+            .then(data => {
+                setDog(data.message);
+                setLoading(false);
+            });
     }, []);
+    
 
     return (
+        loading ? <Spinner /> :
         <Box p={4}>
             <Text fontSize={'24px'} >Dog Gallery</Text>
             <Flex  flexDirection={'row'} gap={2} flexWrap={'wrap'} >
                 {dog.map((dogUrl, index) => (
-                    <Box key={index} maxW={'350px'} borderRadius={'10px'} border={'1px solid gray'} maxH={'650px'} m={2}>
+                    <Box key={index} maxW={'350px'} boxShadow={'rgba(255, 57, 40, 1) 0px 2px 4px, rgba(255, 57, 40, 1) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset'} _hover={{ transform: 'scale(1.1)'}} borderRadius={'10px'} border={'1px solid gray'} maxH={'650px'} m={2}>
                         <Image  borderRadius={'10px'}  border={'1px solid gray'} src={dogUrl} alt="dogs" />
                     </Box>
                 ))}
